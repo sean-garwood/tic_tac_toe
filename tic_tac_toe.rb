af4137ddb8cb460586656e661d133e97b2b2f195
seansg <seansgarwood@gmail.com>
2023-08-02 22:57:49 -0400

    Refactor Turn into Move

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,10 +2,6 @@
 
 # check legality, do other stuff
 module MoveHelper
-  def illegal?
-    # check to see if the move is legal
-  end
-
   def random_move
     # pick a random free square
     # set game board to letter unless move.illegal?
@@ -34,10 +30,6 @@ class Game
     # check to see if the game is a tie
     # if moves = 9 and
   end
-
-  def player_wins?
-    # check if player wins
-  end
 end
 
 # all things relating to the game board
@@ -56,33 +48,30 @@ class Board
   end
 end
 
-# create an object that has player, letter, position to play in
-class Turn
-  attr_accessor :player
-
-  def initialize(player)
-    @player = player
-  end
-end
-
 # move object
-class Move < Turn
+class Move
   include MoveHelper
-  def initialize(user, row, col)
-    @user = user
+  def initialize(player, row, col)
+    @player = player
     @row = row
     @column = col
   end
-end
 
+  def illegal?
+    # check to see if the move is legal
+  end
+end
 
 # Represents state of each players. Initializes two Player objs/game: human and
 # computer.
 class Player
-  attr_reader :winner
+  attr_reader :name
 
-  def initialize(name, winner = false)
+  def initialize(name)
     @name = name
   end
 
+  def wins?
+    @winner
+  end
 end
