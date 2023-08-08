6df205dd2bef4ccdc4b87712e529d225a0cc5716
seansg <seansgarwood@gmail.com>
2023-08-08 01:09:25 -0400

    Implement turns

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,27 +1,57 @@
 # frozen_string_literal: true
 
+# does boring stuff re: turns
 module TurnHelper
   def player_turn
-    @turn_number.odd? ? @player_one.name : @player_two.name
+    turn_number.odd? ? player_one.name : player_two.name
+  end
+
+  def report
+    puts "-=-TURN #{@turn_number}-=-\nGo ahead, #{player_turn}"
   end
 end
 
-def report
-  player_turn
-  puts "-=-TURN #{@turn_number}-=-\nGo ahead, #{player_turn}"
+# all things relating to the game board
+class Board
+  NEW_BOARD = Array.new(3) { Array.new(3, '[ ]') }
+
+  attr_reader :board
+
+  def initialize
+    @board = NEW_BOARD
+  end
+
+  def mark_square(row, col, letter)
+    @board[row][col] = "[#{letter}]" unless @board[row][col] != '[ ]'
+  end
+
+
+  def print_board
+    @board.each do |row|
+      puts row.map { |e| e }.join(' ')
+    end
+  end
+
+  protected
+
+  attr_writer :board
 end
 
 # initializes the game, declares winner
 class Game
   attr_reader :player_one, :player_two, :turn_number, :winner
 
-  includes TurnHelper
+  include TurnHelper
 
   def initialize(player_one, player_two)
     @player_one = player_one
     @player_two = player_two
     @turn_number = 1
     @winner = nil
+  end
+
+  def bump_turn_number
+    @turn_number += 1
   end
 
   private
@@ -40,60 +70,55 @@ class Game
   def cats_game
     # puts something about a tie game and no one wins.
   end
-  end
-
-# all things relating to the game board
-class Board
-  attr_reader :board
-
-  def initialize
-    @board = Array.new(3) { Array.new(3, '[ ]') }
-  end
-
-  def mark_square(row, col, letter)
-    @board[row][col] = "[#{letter}]" unless @board[row][col] != '[ ]'
-  end
-
-  def print_board
-    @board.each do |row|
-      puts row.map { |e| e }.join(' ')
-    end
-  end
-
-  private
-
-  attr_writer :board
 end
 
-# Represents state of each player. Initializes two Player objs/game: human and
-# computer.
+# Represents state of each player. Initializes two Player objs/game
 class Player
-  attr_reader :name
+  attr_reader :letter, :name
 
   @@players = 1
 
   def initialize
-    @name = username
+    @letter = player_letter
+    @name = create_username
     @@players += 1
   end
 
   private
 
-  def username
+  attr_writer :letter
+
+  def player_letter
+    @@players.odd? ? 'x' : 'o'
+  end
+
+  def create_username
     puts "Welcome, player #{@@players}! Enter your name:"
     gets.chomp
   end
+
 end
 
-board = Board.new
+def take_turn(player, board, game)
+  game.report
+  board.print_board
+  puts 'Make your move. Enter the desired row.'
+  row = gets.chomp.to_i
+  puts 'Enter the column.'
+  col = gets.chomp.to_i
+  board.mark_square(row, col, player.letter)
+  game.bump_turn_number
+end
+
 player_one = Player.new
 player_two = Player.new
+board = Board.new
 game = Game.new(player_one, player_two)
 puts 'Game on! Good luck, players.'
-game.report
+puts player_one.name; puts player_two.name
 board.print_board
-puts 'Make your move. Enter the desired row.'
-row = gets.chomp
-puts 'Enter the column.'
-col = gets.chomp
-board.mark_square(row, col, )
+
+while game.turn_number <= 9 do
+  take_turn(player_one, board, game)
+  take_turn(player_two, board, game)
+end
